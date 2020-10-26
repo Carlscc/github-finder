c25899384ba7dedf450382ef8218890a670684d3
Caroline Chan <carolinechanccc@gmail.com>
2020-10-26 11:21:23 +0000

    add search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
 
-export class  extends Component {
+class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    onChange = e => this.setState({ text: e.target.value });
+
+
     render() {
         return (
             <div>
-                
+                <form onSubmit={this.onSubmit} className='form'>
+                    <input
+                     type='text'
+                     name='text'
+                     placeholder='Search Users...' />
+                     value={this.state.text}
+                    <input type='submit' value='Search' className='btn btn-dark btn-block' />
+                </form>
             </div>
         )
     }
 }
 
-export default 
+export default Search;
