7e17acc155899b9df69da5dc03690392fef64a9a
Caroline Chan <carolinechanccc@gmail.com>
2020-11-02 12:34:05 +0000

    search users and clear users from state

--- src/App.js
+++ src/App.js
@@ -9,22 +9,36 @@ class App extends Component {
   state = {
     users: [],
     loading: false
-  }
-  async componentDidMount() {
+  };
+
+  //Search GitHub users
+  searchUsers = async text => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/users?client_id=$
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    this.setState({ users: res.data, loading: false})
-  }
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false});
+  };
+
+  //Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false})
+
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className='app'>
         <nav className='navbar bg-primary'>
           <Navbar title='Github Finder' icon='fab fa-github' />
         </nav>
         <div className='container'>
-          <Search />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true: false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
