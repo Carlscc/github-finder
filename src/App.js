4172fc7795becb1b60282f6e9f1ebce438dc5312
Caroline Chan <carolinechanccc@gmail.com>
2020-11-04 16:05:28 +0000

    create user component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -39,7 +40,7 @@ class App extends Component {
 
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
     return (
       <Router>
       <div className='app'>
@@ -63,11 +64,10 @@ class App extends Component {
                     <Users loading={loading} users={users} />
                 </Fragment>
               )} />
-              <Route
-                exact
-                path='/about'
-                component={About}
-              />
+              <Route exact path='/about'component={About} />
+              <Route exact path='/users/:login' render={props => (
+                <User {...props } getUser={this.getUser} user={user} loading={loading} />
+              )} />
            </Switch>
         </div>
       </div>
