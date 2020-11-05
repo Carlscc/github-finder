1a752b59d8e9384dd7e98bce28bb3a6799b6213b
Caroline Chan <carolinechanccc@gmail.com>
2020-11-05 08:23:49 +0000

    add getuser method, complete user component ui and layout

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -26,6 +27,17 @@ class App extends Component {
 
     this.setState({ users: res.data.items, loading: false});
   };
+
+  //Get single GitHub user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?&client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false});
+  }
 
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false})
@@ -65,7 +77,7 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path='/about'component={About} />
-              <Route exact path='/users/:login' render={props => (
+              <Route exact path='/user/:login' render={props => (
                 <User {...props } getUser={this.getUser} user={user} loading={loading} />
               )} />
            </Switch>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class User extends Component {
     componentDidMount() {
@@ -25,6 +26,7 @@ export class User extends Component {
             html_url,
             followers,
             following,
+            company,
             public_repos,
             public_gists,
             hireable
@@ -34,7 +36,53 @@ export class User extends Component {
 
         if(loading) return <Spinner />
 
-        return <Fragment><h1>{name}</h1></Fragment>
+        return <Fragment>
+            <Link to='/' className='btn btn-light'>Back To Search</Link>
+            Hireable: { ' ' }
+            {hireable ? (
+            <i className='fas fa-check text-success'></i>
+            ) : (
+            <i className='fas fa-times-circle text-danger'></i>
+            )}
+            <div className='card grid-2'>
+                <div className='all-center'>
+                    <img src={avatar_url} className='round-img' style={{ width: '150px' }} alt='' />
+                    <h1>{name}</h1>
+                    <p>{location}</p>
+                </div>
+                <div>
+                    {bio && (<Fragment>
+                        <h3>Bio</h3>
+                        <p>{bio}</p>
+                        </Fragment>
+                    )}
+                    <a href={html_url} className='btn btn-dark my-1'>Visit GitHub Profile</a>
+                    <ul>
+                        <li>
+                            {login && (<Fragment>
+                                <strong>Username: </strong> {login}
+                                </Fragment>)}
+                        </li>
+                        <li>
+                            {company && (<Fragment>
+                                <strong>Company: </strong> {company}
+                                </Fragment>)}
+                        </li>
+                        <li>
+                            {blog && (<Fragment>
+                                <strong>Website: </strong> {blog}
+                                </Fragment>)}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className='card text-center'>
+                <div className='badge badge-primary'>Followers: {followers}</div>
+                <div className='badge badge-success'>Following: {following}</div>
+                <div className='badge badge-light'>Public Repos: {public_repos}</div>
+                <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+            </div>
+        </Fragment>;
     }
 }
 
